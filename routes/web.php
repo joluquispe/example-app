33a368581a4dc003d574d2dd1ac6e8594cb1af83
jolu <joluquispe2@gmail.com>
2023-01-16 13:56:20 -0500

    se empezó a configurar la rutas para la web

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,12 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-// Route::get('/', function () {
-//     return 'HOME PAGE';
-// });
-
-// Route::get('blog', function () {
-//     return 'Lista de Publicaciones';
-// });
+ Route::get('/', function () {
+    return 'HOME PAGE';
+});
+ Route::get('blog', function () {
+    return 'Lista de Publicaciones';
+});
