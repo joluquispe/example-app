e5c64974506657356a9165bf43a19b52c1c7b947
jolu <joluquispe2@gmail.com>
2023-01-18 12:36:09 -0500

    se modificó vista dinamica a los blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,9 +4,9 @@
     <h1>Listado</h1>
     @foreach ($posts as $post)
     <p>
-        <strong>{{ $post['id'] }}</strong>
-        <a href="{{ route('post', $post['slug']) }}">
-            {{ $post['title'] }}
+        <strong>{{ $post->id }}</strong>
+        <a href="{{ route('post', $post->slug) }}">
+            {{ $post->title }}
         </a>
     </p>
     @endforeach
