1936ef4cc94b6fdf6b7e8174bc87dde32bb8d4a6
jolu <joluquispe2@gmail.com>
2023-01-18 15:23:45 -0500

    dasf

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 /** 
 Route::get('/', [PageController::class, 'home'])->name('home');
-
-Route::get('blog', [PageController::class, 'blog'])->name('blog');
-    
+Route::get('blog', [PageController::class, 'blog'])->name('blog');    
 Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
 */
 
