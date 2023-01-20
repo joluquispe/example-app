d4db9df63e4aeab1e277cca00b1fd727f9926d98
jolu <joluquispe2@gmail.com>
2023-01-19 23:11:18 -0500

    se instaló inicio de sesion

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,31 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route::get   | Consultar
- * Route::post  | Guardar
- * Route::delete| Eliminar
- * Route::put   | Actualizar
- */
-
-/** 
-Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('blog', [PageController::class, 'blog'])->name('blog');    
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
 */
 
-Route::controller(PageController::class)->group(function () {
-    Route::get('/',                'home')->name('home');
-    Route::get('blog',             'blog')->name('blog');        
-    Route::get('blog/{post:slug}', 'post')->name('post'); 
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
